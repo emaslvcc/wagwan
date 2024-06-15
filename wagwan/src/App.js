b684eb01e8796881d77034688dbb5eb68aa673e0
emaslvcc <emadasilvachagas@gmail.com>
2024-06-15 11:02:01 +0200

    emas branch

--- wagwan/src/App.js
+++ wagwan/src/App.js
@@ -1,8 +1,44 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState } from 'react';
+import axios from 'axios';
+import './App.css';
+import logo from './logo192.png';
 
 function App() {
-  return <h1>Hello World!</h1>;
+  const [employeeID, setEmployeeID] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:5000/login', { employeeID });
+      alert(response.data);
+    } catch (error) {
+      console.error('Error:', error);
+      alert('An error occurred while submitting the login form.');
+    }
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div className="login-container">
+          <h1>Login to Precedence BV</h1>
+          <p>Enter your employee details to login</p>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              value={employeeID}
+              onChange={(e) => setEmployeeID(e.target.value)}
+              placeholder="Employee ID (eg., e6352037)"
+              required
+            />
+            <button type="submit">Login</button>
+          </form>
+        </div>
+      </header>
+    </div>
+  );
 }
 
 export default App;
+
